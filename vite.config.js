072d96829e7df9289b706efa9d54b251c462e26a
minhngo-upp <minhngo.neu@gmail.com>
2026-02-26 15:21:38 +0700

    Fix blank GitHub Pages site with relative base and deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages project site serves at /<repo>/, not /
-  // Repo name here is Test -> https://<user>.github.io/Test/
-  base: '/Test/',
+  // Use relative asset paths so project sites like /Test/ don't break.
+  base: './',
 })
